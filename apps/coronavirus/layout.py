228d0e14a33de924e3f7ac3e8199875c5b9e27b3
erikakert <akerterik@gmail.com>
2020-03-22 15:12:51 -0600

    Update layout.py

--- apps/coronavirus/layout.py
+++ apps/coronavirus/layout.py
@@ -17,7 +17,6 @@ from app import cache
 
 
 def get_coronavirus_data(session_id):
-    @cache.memoize()
     def pull_and_serialize_data(session_id):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
